c991ea9f3ea27b2e69fef258ebe7dd8ae1b990f0
umezawa.kanta <kanta0930@gmail.com>
2019-01-25 04:36:14 +0900

    consuming-rest added

---
+++ consuming-rest/src/test/java/hello/ApplicationTest.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+    @Test
+    public void restTemplate() {
+    }
+
+    @Test
+    public void run() {
+    }
+}
